c38a3ad20030eac5d184327f6da2497a665fc4a0
gelserkirchen <gelserkirchen@mail.ru>
2019-12-23 08:49:56 +0300

    Initial Commit

---
+++ SecondProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondProject
+//
+//  Created by Андрей Толстых on 23.12.2019.
+//  Copyright © 2019 Андрей Толстых. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
